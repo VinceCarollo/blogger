926b78ac43469b32ee9bb3ade16c484bde40f38e
Vince Carollo <vincecarollo@gmail.com>
2019-05-04 14:43:50 -0600

    Change test to reflect correct wording for rails form creation

--- spec/features/user_can_edit_articles_spec.rb
+++ spec/features/user_can_edit_articles_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'As a visitor' do
 
         fill_in 'article[title]', with: "New Title"
         fill_in 'article[body]', with: "New Body"
-        click_on "Edit Article"
+        click_on "Update Article"
 
         expect(current_path).to eq(article_path(Article.last))
         expect(page).to have_content('New Title')
